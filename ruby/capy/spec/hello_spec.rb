3ee790092d2b4c8f06c66ceacde311404bd10baa
Jilvan Pereira <jilvan.pereira@gold360.local>
2019-09-03 18:49:26 -0300

    Resizing window

--- ruby/capy/spec/hello_spec.rb
+++ ruby/capy/spec/hello_spec.rb
@@ -1,9 +1,13 @@
 
 
-describe 'Meu primeiro script' do
+describe 'Meu primeiro script', :hello do
+
+    before(:each) do #tamanho da janela
+        page.current_window.resize_to(1280, 800)
+    end
 
     it 'visitar a página' do
-        visit ''
+        visit '/'
         expect(page.title).to eql 'Training Wheels Protocol'
     end
 end
